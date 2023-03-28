95c54574c3d84548272429594d5810106ac3872d
Elexis-biz <elekwachidinma1@gmail.com>
2023-03-28 03:44:15 -0700

    initial push

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 function that takes a pointer to an int as parameter and updates
+ * reset_to_98 - function that takes a pointer to an int as parameter
  * @n: the value to be updated to 98
  * Return: Always 0
  */
- 
+
 void reset_to_98(int *n)
 {
-	n = 98;
+	*n = 98;
 }
- 
